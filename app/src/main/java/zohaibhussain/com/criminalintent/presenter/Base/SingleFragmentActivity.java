1d488e5a2e3765a0c28bba3565c09d4d637230bb
Zohaib-AHussain <zowaib@gmail.com>
2015-12-16 09:42:00 -0500

    setting up fragments for hosting recycler view refactor activity to make it generic - SingleFragmentActivity changed the launching activity to list

---
+++ app/src/main/java/zohaibhussain/com/criminalintent/presenter/Base/SingleFragmentActivity.java
@@ -0,0 +1,29 @@
+package zohaibhussain.com.criminalintent.presenter.Base;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import zohaibhussain.com.criminalintent.R;
+
+/**
+ * Created by zohaibhussain on 2015-12-15.
+ */
+public abstract class SingleFragmentActivity extends FragmentActivity {
+    
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment==null)
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, createFragment());
+    }
+
+
+    protected abstract Fragment createFragment();
+}
---
+++ app/src/main/java/zohaibhussain/com/criminalintent/view/CrimeListFragment.java
@@ -0,0 +1,10 @@
+package zohaibhussain.com.criminalintent.view;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by zohaibhussain on 2015-12-15.
+ */
+public class CrimeListFragment extends Fragment {
+}
