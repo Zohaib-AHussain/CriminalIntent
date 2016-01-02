9b91c69621591f7201d7c937734bbd98cdeca5aa
Zohaib-AHussain <zowaib@gmail.com>
2016-01-01 19:01:44 -0500

    -Adding in method to generate runtime crime report -Adding in strings that can be formatted at runtime

--- app/src/main/java/zohaibhussain/com/criminalintent/view/CrimeFragment.java
+++ app/src/main/java/zohaibhussain/com/criminalintent/view/CrimeFragment.java
@@ -121,6 +121,25 @@ public class CrimeFragment extends Fragment {
         return crimeFragment;
     }
 
+    private String getCrimeReport(){
+        String solvedString = null;
+        if (mCrime.isSolved())
+            solvedString = getString(R.string.crime_report_solved);
+        else
+            solvedString = getString(R.string.crime_report_unsolved);
+
+        String dateString = DateUtil.getFormattedDate(mCrime.getDate());
+        String suspect = mCrime.getSuspect();
+        if (suspect == null)
+            suspect = getString(R.string.crime_report_no_suspect);
+        else
+            suspect = getString(R.string.crime_report_suspect, suspect);
+
+        String report = getString(R.string.crime_report, mCrime.getTitle(), dateString, solvedString, suspect);
+        return report;
+
+    }
+
     @Override
     public void onPause(){
         super.onPause();
